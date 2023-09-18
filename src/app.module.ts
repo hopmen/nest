7952fffe87be54c10f40f70ad6abd8b0980c1551
hopman <3tonchik@mail.ru>
2023-09-18 23:31:53 +0300

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { RoomModule } from './room/room.module';
+import { ЕшtimetableModule } from './ешtimetable/ешtimetable.module';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule, RoomModule, ЕшtimetableModule],
   controllers: [AppController],
   providers: [AppService],
 })
