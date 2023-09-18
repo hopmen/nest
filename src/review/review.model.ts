4b3b6971777cd8c8867508c34c47b6ade1b90cc1
hopman <3tonchik@mail.ru>
2023-09-18 22:36:35 +0300

    add controller

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,7 +1,8 @@
 export class ReviewModel {
-  name:string;
-  title:string;
-  description:string;
-  rating:number;
-  CreatedAt: Date;
+	_id: string;
+	name: string;
+	title: string;
+	description: string;
+	rating: number;
+	CreatedAt: Date;
 }
